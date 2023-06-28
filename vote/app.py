a0aefa389d2b450166b34bfb8668d3a5bc9a092d
mainak6434 <mainak6434@yahoo.co.in>
2023-06-28 19:06:50 +0100

    get redis password from env

--- vote/app.py
+++ vote/app.py
@@ -8,6 +8,7 @@ import logging
 
 option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
+redisPassword = os.getenv('REDIS_PASSWORD', "")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
@@ -18,7 +19,7 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5, password='redis_password')
+        g.redis = Redis(host="redis", db=0, socket_timeout=5, password = redisPassword)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
